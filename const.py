b1133dc832d9a9a5d6595db09df7e5c1ede9d74a
Golubev-Vasiliy <vase4ka346@gmail.com>
2017-11-26 18:40:18 +0100

    Give me music
    Bot has one command - /music
    This command receive Russian music to a customer!

---
+++ const.py
@@ -0,0 +1,2 @@
+token = '452787256:AAEr8gS8I8cGpH9oAfRXIog7ip6q_XQuagk'
+
---
+++ main.py
@@ -0,0 +1,83 @@
+import const
+import logging
+import random
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s', level=logging.INFO)
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a fucking bot, please talk to me!\n"
+                                                          "I know only one command because I am very stupid!\n"
+                                                          "    /music")
+
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry man, you must use special commands for conversation "
+                                                          "with me!\n Write /help for receive a help.")
+
+
+def help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="You can use next commands:\n"
+                                                          "    /music")
+
+
+def caps(bot, update, args):
+    text_caps = ' '.join(args).upper()
+    bot.send_message(chat_id=update.message.chat_id, text=text_caps)
+
+
+def music(bot, update):
+    filename = str(random.randint(1, 3))
+    filenamemusic = filename + ".mp3"
+    filenamephoto = filename + ".jpg"
+    bot.send_message(chat_id=update.message.chat_id, text="Music for Russian\n Wait please!")
+    bot.send_audio(chat_id=update.message.chat_id, audio=open(filenamemusic, 'rb'), duration=20, timeout=60)
+    bot.send_photo(chat_id=update.message.chat_id, photo=open(filenamephoto, 'rb'))
+
+
+def photo(bot, update):
+    filename = "2.jpg"
+    bot.send_message(chat_id=update.message.chat_id, text=filename)
+    bot.send_photo(chat_id=update.message.chat_id, photo=open(filename, 'rb'))
+
+
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command")
+
+
+def main():
+    updater = Updater(const.token)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help)
+    echo_handler = MessageHandler(Filters.text, echo)
+    caps_handler = CommandHandler('caps', caps, pass_args=True)
+    music_handler = CommandHandler('music', music)
+    photo_handler = CommandHandler('photo', photo)
+    unknown_handler = MessageHandler(Filters.command, unknown)
+
+    # добавление обработчиков
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(caps_handler)
+    dispatcher.add_handler(music_handler)
+    dispatcher.add_handler(photo_handler)
+    dispatcher.add_handler(unknown_handler)
+
+    # запуск работы обработчиков
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
